df5ce5fb479679d19de442af36e28d47559a3ae5
keith <kchester@gmail.com>
2023-10-19 11:27:12 -0700

    feature: parameters for service

--- ros_ws/src/llm/llm/service.py
+++ ros_ws/src/llm/llm/service.py
@@ -1,24 +1,52 @@
 import rclpy
-from rclpy.node import Node
-from capstone_interfaces.srv import LLM
-
 from llm.providers import OpenAI, PaLM
+from rcl_interfaces.msg import ParameterDescriptor
+from rclpy.node import Node
+
+from capstone_interfaces.srv import LLM
 
 
 class LLMService(Node):
-    def __init__(self, provider):
+    def __init__(self):
         super().__init__("llm_service")
 
-        # Determine the provider; this will crashif an environment variable
-        # for the provider is not set
-        if provider == "openai":
-            self.provider = OpenAI(None, None)
-        elif provider == "palm":
-            self.provider = PaLM(None, None)
-        else:
-            raise ValueError(f"Unknown provider {provider}")
+        self.initialize_parameters()
 
         self.srv = self.create_service(LLM, "prompt", self.prompt_callback)
+
+    def initialize_parameters(self) -> None:
+        self.declare_parameter(
+            "provider",
+            "",
+            ParameterDescriptor(
+                description="Which LLM provider (openai or palm) to use"
+            ),
+        )
+
+        self.declare_parameter(
+            "model",
+            "",
+            ParameterDescriptor(
+                description="Which LLM model to use (provider dependent - leave blank for default)"
+            ),
+        )
+
+        self.provider = (
+            self.get_parameter("provider").set_parameter_value().string_value
+        )
+        self.model = self.get_parameter("model").set_parameter_value().string_value
+
+        print("Hey I got the stuff", self.provider, self.model)
+
+    def initialize_provider(self) -> None:
+        # Determine the provider; this will crash if an environment variable
+        # for the provider is not set
+        if self.provider == "openai":
+            self.provider = OpenAI(None, self.model)
+        elif self.provider == "palm":
+            self.provider = PaLM(None, self.model)
+        else:
+            raise ValueError(f"Unknown provider {self.provider}")
 
     def prompt_callback(self, request: LLM.Request, response: LLM.Response):
         self.get_logger().debug(request.prompt)
@@ -37,7 +65,7 @@ def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
     # declare the node constructor
-    service = LLMService("openai")
+    service = LLMService()
     # pause the program execution, waits for a request to kill the node (ctrl+c)
     rclpy.spin(service)
     # shutdown the ROS communication
