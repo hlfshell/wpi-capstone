0f56a10446cae6baf5b03ce2e2ab3b0d32e22668
Rhodwarrior <86941048+Rhodwarrior@users.noreply.github.com>
2023-11-15 14:35:26 -0800

    updated package needs
    Fixe KC's suggestions in package docs.

--- ros_ws/src/Interpreter_package/setup.py
+++ ros_ws/src/Interpreter_package/setup.py
@@ -4,7 +4,7 @@ package_name = 'Interpreter_package'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='vagrant',
-    maintainer_email='vagrant@todo.todo',
-    description='TODO: Package description',
+    maintainer='Bob DeMont',
+    maintainer_email='rldemont@wpi.edu',
+    description='This interacts with User Node to interpret need',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
