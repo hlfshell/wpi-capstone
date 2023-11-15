c7aa647dcc44684bdb1c73892ee2a512b6fe0610
Brigitte B <brigitte.broszus@gmail.com>
2023-11-15 09:17:45 -0500

    Add implementation of new object client and service. Client still has issues sending data to service to receive. Service can read from command line service call

--- ros_ws/src/query_services/query_services/new_object_client.py
+++ ros_ws/src/query_services/query_services/new_object_client.py
@@ -1,7 +1,12 @@
 from capstone_interfaces.msg import StateObject, ObjectSpotted
-from database_functions import *
+from capstone_interfaces.srv import AddObject
+from query_services import database_functions
+
+import sys
 
 import rclpy
+import time
+from numpy import uint32
 import builtin_interfaces
 from rclpy.node import Node
 
@@ -9,38 +14,64 @@ from rclpy.node import Node
 class SpotNewObjectClient(Node):
 
     def __init__(self):
-        super().__init__('spot_new_object')
-
-
-        self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        super().__init__('spot_new_object_client')
+        self.cli = self.create_client(AddObject, 'add_new_object')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = AddTwoInts.Request()
+        self.req = AddObject.Request()
 
+    def found_object(self, info: ObjectSpotted):# -> StateObject:
+        """
+        Add objects from given object information:
+        : param description
+        :return: all 
+        """
+        self.req.object_info.description = info.description
+        self.req.object_info.x = info.x
+        self.req.object_info.y = info.y
+        self.req.object_info.z = info.z
+        self.req.object_info.time_seen = info.time_seen
 
+        print(type(self.req))
 
-
-        conn = create_connection()
-        if conn is not None:
-            create_object_table(conn)
-            # test query?
-            self.srv = self.create_service(SpotNewObjectService, 'add_new_object', self.add_object)     
-
-    def add_object(self, description: str, location: str, x: float, y: float, z: float, task: str, timestamp:builtin_interfaces/Time) -> list[StateObject]:
-        conn = create_connection()
-        with conn:
-            spotted_object = (description,location,x,y,z,task,timestamp)
-            create_object(conn, spotted_object)
-            # test query?
+        print("TO REMOVE: SENDING REQUEST")
+        print(self.req)
+        print ("REQUEST SENT")
+        self.future = self.cli.call_async(self.req)
+        print("future",self.future.result())
+        print(self)
+        rclpy.spin_until_future_complete(self,self.future)
+        return self.future.result()
 
 
 def main():
     rclpy.init()
 
-    new_object_service = SpotNewObjectService()
+    spot_new_object = SpotNewObjectClient()
 
-    rclpy.spin(new_object_service)
+    obj_info = ObjectSpotted()
+    obj_info.description = str(sys.argv[1])
+    obj_info.x = float(sys.argv[2])
+    obj_info.y = float(sys.argv[3])
+    obj_info.z = float(sys.argv[4])
 
+    time_obj = builtin_interfaces.msg.Time()
+    time_val = time.time()
+    time_obj.sec = int(time_val)
+    time_obj.nanosec = int(uint32((time_val-time_obj.sec)*10e9))
+    obj_info.time_seen = time_obj
+
+    print(obj_info)
+    print(type(obj_info))
+
+    response = spot_new_object.found_object(obj_info)
+ 
+    spot_new_object.get_logger().info(
+        'Object:\n  ID: %d\n  Description: %s\n  Location: %s\n  (x,y,z): (%f,%f,%f)\n  Task: %s\n  Time: %s' %
+        (str(response.object_state.id), response.object_state.description, response.object_state.location, 
+        str(response.object_state.x),str(response.object_state.y),str(response.object_state.z),response.object_state.task_when_seen,str(response.object_state.time_seen)))
+
+    spot_new_object.destroy_node()
     rclpy.shutdown()
 
 
--- ros_ws/src/query_services/query_services/new_object_service.py
+++ ros_ws/src/query_services/query_services/new_object_service.py
@@ -1,5 +1,6 @@
 from capstone_interfaces.msg import StateObject, ObjectSpotted
-import database_functions
+from capstone_interfaces.srv import AddObject
+from query_services import database_functions
 
 import rclpy
 import builtin_interfaces
@@ -9,27 +10,82 @@ from rclpy.node import Node
 class SpotNewObjectService(Node):
 
     def __init__(self):
-        super().__init__('process_new_object')
-        conn = create_connection()
-        if conn is not None:
-            create_object_table(conn)
-            # test query?
-            self.srv = self.create_service(SpotNewObjectService, 'process_new_object', self.add_object)     
+        super().__init__('spot_new_object_service')
+        self.conn = database_functions.create_connection(r"state_db.db")
+        self.srv = self.create_service(AddObject, 'add_new_object', self.add_object)
+        if self.conn is not None:
+            database_functions.create_object_table(self.conn) # checks if already made
 
-    def add_object(self, description: str, location: str, x: float, y: float, z: float, task: str, timestamp:builtin_interfaces/Time) -> list[StateObject]:
-        conn = create_connection()
-        with conn:
-            spotted_object = (description,location,x,y,z,task,timestamp)
-            create_object(conn, spotted_object)
-            # test query?
+        #def add_object(self, incoming_object_info: ObjectSpotted, response:StateObject)->StateObject:
+    def add_object(self, request, response):
+     
+        print("Received:\n",request.object_info)
+        print(type(request))
+
+        object_to_add = StateObject()
+
+        object_to_add.description = request.object_info.description
+        object_to_add.x = request.object_info.x
+        object_to_add.y = request.object_info.y
+        object_to_add.z = request.object_info.z
+        object_to_add.time_seen = request.object_info.time_seen
+        object_to_add.location = self.determine_room(request.object_info.x,request.object_info.y,request.object_info.z)
+        object_to_add.task_when_seen = "TODO: Build link with Mission Control"
+
+        print(object_to_add)
 
 
+        response.object_state.description = object_to_add.description
+        
+        response.object_state.x = object_to_add.x
+        response.object_state.y = object_to_add.y
+        response.object_state.z = object_to_add.z
+        response.object_state.time_seen = object_to_add.time_seen
+
+        response.object_state.location = object_to_add.location
+        response.object_state.task_when_seen = object_to_add.task_when_seen
+        
+        print(response)
+
+        with self.conn:
+            last_row_id = database_functions.create_object(self.conn, object_to_add)
+
+        response.object_state.id = last_row_id
+        if response.object_state.description == "":
+            response.object_state.description = "Element not found"
+
+        self.get_logger().info(
+            'Object:\n  ID: %d\n  Description: %s\n  Location: %s\n  (x,y,z): (%f,%f,%f)\n  Task: %s\n  Time: %s'
+            %(response.object_state.id, response.object_state.description, response.object_state.location,response.object_state.x,response.object_state.y,response.object_state.z,response.object_state.task_when_seen,str(response.object_state.time_seen)))
+
+        print("response:\n",response,"\n",type(response))
+        return response
+
+
+
+    def determine_room(self, x: float, y: float, z: float) -> str:
+        # Depends on simulation room, since we are assuming we have a map apriori
+        # Todo: work with actual simulated home
+
+        if x>0 and x<=10 and y>0 and y<10: # first quadrant (TR)
+            return "Living Room"
+        elif x>-10 and x<=0 and y>0 and y<10: #second quadrant (TL)
+            return "Bathroom"
+        elif x>-10 and x<=0 and y>-10 and y<=0: #third quadrant (BL):
+            return "Bedroom"
+        elif x>0 and x<=10 and y>-10 and y<=0 : #fourth quadrant (BR)
+            return "Kitchen"
+        else:
+            return "outside the house"
+
+
+        
 def main():
     rclpy.init()
 
-    new_object_service = SpotNewObjectService()
+    service = SpotNewObjectService()
 
-    rclpy.spin(new_object_service)
+    rclpy.spin(service)
 
     rclpy.shutdown()
 
