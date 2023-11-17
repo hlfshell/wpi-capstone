5577e4a43e237f15fabab68d12384215aca75b46
Rhodwarrior <86941048+Rhodwarrior@users.noreply.github.com>
2023-11-16 18:37:31 -0800

    Add User Node Package
    This adds the user node package

---
+++ ros_ws/src/user_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'user_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vagrant',
+    maintainer_email='vagrant@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'user = user_package.user:main'
+        ],
+    },
+)
---
+++ ros_ws/src/user_package/user_package/user.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from user_package.')
+
+
+if __name__ == '__main__':
+    main()
