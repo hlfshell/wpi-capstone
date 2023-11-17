34e6a8728e8c0c8002f562460aeb207266beda52
Rhodwarrior <86941048+Rhodwarrior@users.noreply.github.com>
2023-11-16 19:02:35 -0800

    added user node code
    added code to pub sub for user node

--- ros_ws/src/user_package/user_package/user.py
+++ ros_ws/src/user_package/user_package/user.py
@@ -1,5 +1,32 @@
-def main():
-    print('Hi from user_package.')
+import os
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class User_Node():
+    def __init__(self):
+        super().__init__('interpreter')
+        self.request_publisher_=self.create_publisher(String,'user_request',10)
+        self.chat_publisher_=self.create_publisher(String, 'cq_response',10)
+        #define subscriptions
+        self.subscriber_=self.create_subscription(String, 'clarifying_question',self.get_question_and_respond)
+
+    def initiate_request(self):
+        request=input('What would you like?')
+        self.request_publisher_.publish(request)
+
+    def get_question_and_respond(self, question:str):
+        print(question))
+        response=input('?:')
+        self.chat_publisher_(response)
+    
+def main(args=None):
+    rclpy.init(args=args)
+    user=User_Node()
+    user.initiate_request()
+    rclpy.spin()
+    user.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
