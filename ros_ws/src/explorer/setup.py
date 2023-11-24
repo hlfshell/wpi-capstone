dba2ddf3b0badbda17713e0839762e1c6ea19146
Keith Chester <kchester@gmail.com>
2023-11-24 09:59:03 -0800

    chore: setup filepaths and entrypoints

--- ros_ws/src/explorer/setup.py
+++ ros_ws/src/explorer/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "explorer"
@@ -9,6 +12,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -18,6 +23,8 @@ setup(
     license="MIT",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [],
+        "console_scripts": [
+            "explorer = explorer.service:main",
+        ],
     },
 )
