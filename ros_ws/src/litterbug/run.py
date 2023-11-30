ac184ec340b3325b0282194359de29832fdc5650
Keith Chester <kchester@gmail.com>
2023-11-30 10:43:06 -0800

    chore: snapshot of debug test code

--- ros_ws/src/litterbug/run.py
+++ ros_ws/src/litterbug/run.py
@@ -2,6 +2,7 @@ from litterbug.map import Map
 from litterbug.litterbug import Litterbug
 from litterbug.items import Item
 import rclpy
+from time import sleep
 
 rclpy.init()
 
@@ -15,14 +16,29 @@ rclpy.init()
 map = Map.FromMapFile("remap", (-0.5, -2.0))
 # map.FromOccupancy()
 
+
+items = Item.from_csv("./test.csv")
+
 print("creating litterbug")
 lb = Litterbug(
-    [
-        Item("can", "can", (-0.052102, 1.827700, 0.5), (0, 0, 0, 0)),
-        # Item("can2", "can2", (1.8277, -0.052102, 0.5), (0, 0, 0, 0)),
-        Item("can2", "can2", (-0.062137, -1.826920, 0.5), (0, 0, 0, 0)),
-    ],
+    # [
+    #     Item("can", "can", (-0.052102, 1.827700, 0.5), (0, 0, 0, 0)),
+    #     # Item("can2", "can2", (1.8277, -0.052102, 0.5), (0, 0, 0, 0)),
+    #     Item("can2", "can2", (-0.062137, -1.826920, 0.5), (0, 0, 0, 0)),
+    # ],
+    items,
     map,
 )
-# lb.test()
+lb.wait_for_ready()
+lb.populate()
+
+# print(items[0])
+
+# rclpy.spin(lb)
+
+# while True:
+#     sleep(1.0)
+#     try:
+#         lb.__pickup_object()
+
 rclpy.spin(lb)
