20c5227b37278033357387d76484440e75f9f97f
Rhodwarrior <86941048+Rhodwarrior@users.noreply.github.com>
2023-12-04 13:40:22 -0800

    Single node with context enhancements

--- ros_ws/src/interpreter_pkg/interpreter_pkg/interpreter.py
+++ ros_ws/src/interpreter_pkg/interpreter_pkg/interpreter.py
@@ -8,7 +8,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate,
                                SystemMessagePromptTemplate)
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from rclpy.node import Node
 
 data_dir = os.path.join(get_package_share_directory("interpreter_pkg"))
@@ -31,24 +31,26 @@ class LLM_Object:
         with open(os.path.join(data_dir, context_file), 'r') as file:
             context = file.read().replace('\n', '')
         self.context = context
-        self.rolling_context = self.context
         self.temp = temperature
         self.chat = ChatOpenAI(model_name=model)
         self.system_message = SystemMessage(content=self.context)
+        self.rolling_context = [self.system_message]
 
     def reset_context(self):
         self.rolling_context = [self.system_message]
 
 
 class InterpreterNode(Node):
+
     '''This is the iterpreter node designed to interpret
-    va chat if necessary the target object that the user
+    via chat if necessary the target object that the user
     wants.  It publishes the target_message.  As part of user 
     inpout, it subscribes to user_request and in response
     publishes clarifying_question if necessary'''
+
     def __init__(self):
         super().__init__('interpreter')
-        self.get_logger().info("Terp Started")
+        # self.get_logger().info("Terp Started")
         # define elements
         self.refiner = LLM_Object("refiner_context.prompt", MODEL_TO_USE, 0)
         self.judger = LLM_Object("judge_context.prompt", MODEL_TO_USE, 0)
@@ -57,74 +59,73 @@ class InterpreterNode(Node):
         self.new_request = True
         self.attempts_to_understand = 0
         self.target = ""
-        self.rolling_context = []
         self.last_suggestion = ""
+
         # define publishing
         self.target_publisher_ = self.create_publisher(String, 'target', 10)
-        self.chat_publisher_ = self.create_publisher(String, 'clarifying_question', 10)
-        # define subscriptions
-        self.subscriber_ = self.create_subscription(String, 'user_request', self.get_request, 10)
+        self.target_id_loop()
 
-    def get_request(self, request_msg: String):
+    def target_id_loop(self):
+        while True:
+            request = self.initiate_request()
+            while self.target == "":
+                request = self.get_request(request)
+            self.broadcast_target(self.target)
+
+    def initiate_request(self):
+        request = input('Robot: What can I get for you?')
+        return request
+
+    def get_request(self, request: str):
         '''
-        THis is the callback for subscribing to user response.
-        This callback gets the user response to a chat string to
+        This gets the user response to a chat string to
         check if the response is yes-that means user is confirming
         your last item, if so, the last ai message is the target.
         '''
-        request = request_msg.data
+        response = ""
 
         if request.strip().lower() == "yes" and self.new_request is False:
-            # user is responding to a direct suggestion
-            self.target = extract_request(self.refiner.last_suggestion)
-            self.broadcast_target(self.target)
-
-        self.new_request = False
-        # So if not responding yes, use judge to see if the item is
-        # actionable in response
-        judgement = judge(self.judger, request)
-        # some debugging lines to check if the judge is getting correct context
-        # self.get_logger().info("data directroy is :"+data_dir)
-        # self.get_logger().info("judge context is: "+self.judger.context)
-        # self.get_logger().info("judgement = " + judgement)
-        if judgement == "yes":
-            # identify simple target
-            self.target = extract_request(self.extractor, request)
-            print(self.target)
-            # publish target
-            self.broadcast_target(self.target)
-
-        # if we've been going round, restart conversation
-        elif self.attempts_to_understand == 5:
-            # reset rolling context
-            self.refiner.reset_context()
-            self.attempts_to_understand = 0
-            # Chat the request to start over
-            start_over_msg = String()
-            start_over_msg.data = "I'm sorry i dont inderstand. Could we start over?"
-            self.chat_publisher_.publish(start_over_msg)
+            # user is responding to a direct suggestion not making a first request
+            self.target = extract_request(self.extractor, self.last_suggestion)
         else:
-            # we dont know what the target is yet so ask another question
-            self.ask_clarifying_question(request)
+            self.new_request = False
+            # So if not responding yes, use judge to see if the item is
+            # actionable in request
+            judgement = judge(self.judger, request)
+            if judgement == "yes":
+                # identify simple target
+                self.target = extract_request(self.extractor, request)
+                
+            # if we've been going round, restart conversation
+            elif self.attempts_to_understand == 5:
+                # reset rolling context
+                self.refiner.reset_context()
+                self.attempts_to_understand = 0
+                # Chat the request to start over
+                print("Robot: I'm sorry i dont inderstand. Could we start over?")
+            else:
+                # we dont know what the target is yet so ask another question
+                response = self.ask_clarifying_question(request)
+            return response
 
     def ask_clarifying_question(self, user_msg: str):
-        self.rolling_context.append(HumanMessage(content=user_msg))
+        self.refiner.rolling_context.append(HumanMessage(content=user_msg))
         # LLM call to generate clarifying qustion
-        ai_question_to_user = self.refiner.chat(self.rolling_context)
+        ai_question_to_user = self.refiner.chat(self.refiner.rolling_context)
         self.last_suggestion = ai_question_to_user.content
-        self.get_logger().info("The response from the LLM is "+ai_question_to_user.content)
-        self.rolling_context.append(ai_question_to_user.content)
+        # self.get_logger().info("The response from the LLM is "+ai_question_to_user.content)
+        self.refiner.rolling_context.append(AIMessage(content=ai_question_to_user.content))
         self.attempts_to_understand += 1
-        # publish the question to user
-        clarifying_question = String()
-        clarifying_question.data = ai_question_to_user.content
-        self.chat_publisher_.publish(clarifying_question)
+        # clarifying question for user
+        response = input("Robot: "+ai_question_to_user.content)
+        self.last_suggestion = ai_question_to_user.content
+        return response
 
     def broadcast_target(self, target: str):
         '''
         callback for publishing target name
         '''
-        self.get_logger().info("The target is " + self.target)
+        self.get_logger().info("Broadcasting target: " + self.target)
         target_msg = String()
         target_msg.data = target
         self.target_publisher_.publish(target_msg)
@@ -134,8 +135,8 @@ class InterpreterNode(Node):
         self.new_request = True
         self.attempts_to_understand = 0
         self.target = ""
-        self.rolling_context = []
         self.refiner.reset_context()
+        self.last_suggestion = ""
 
 
 def judge(judger: LLM_Object, request: String) -> str:
