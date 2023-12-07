6200ee17fdc85da0bb411a2e68a4d8876e1514d3
clintubuntu <clinton.bill.williams@gmail.com>
2023-12-06 20:15:07 -0500

    fetch nav param and launch file updates

--- ros_ws/src/fetch_description/launch/fetch.navigation.launch.py
+++ ros_ws/src/fetch_description/launch/fetch.navigation.launch.py
@@ -1,50 +1,177 @@
 import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
-import os
 
 from ament_index_python.packages import get_package_share_directory
-
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessStart, OnProcessExit
+from launch_ros.actions import Node
+import launch_ros
+import os
+import xacro
+# TODO get nav working with fetch and fix camera
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='fetch_description').find('fetch_description')
     default_model_path = os.path.join(pkg_share, 'robots/fetch.urdf')
-    config_dir = os.path.join(pkg_share, "config")
-    default_rviz_config_path = os.path.join(config_dir, 'fetch_urdf_config.rviz')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
 
-    robot_state_publisher_node = launch_ros.actions.Node(
+    params_file = os.path.join(pkg_share, "config","nav2_params.yaml")
+    
+    house_world_path = os.path.join(
+        get_package_share_directory("aws-robomaker-small-house-world"),
+        "worlds",
+        "small_house.world",
+    )
+    map_file = os.path.join(
+        get_package_share_directory("aws-robomaker-small-house-world"),
+        "maps",
+        "map.yaml",
+    )
+
+    x_pose = LaunchConfiguration("x_pose", default="0.0")
+    y_pose = LaunchConfiguration("y_pose", default="0.0")
+
+    doc = xacro.parse(open(default_model_path))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, "launch", "gzserver.launch.py")
+        ),
+        launch_arguments={"world": house_world_path}.items(),
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, "launch", "gzclient.launch.py")
+        )
+    )
+
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        output='screen',
+        parameters=[robot_description, {'use_sim_time': True}]
     )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_fetch_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'fetch_controller'],
+        output='screen'
+    )
+
+    # need to provide urdf file to joint_state_publisher
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        parameters=[{'use_sim_time': True}]
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+
+    robot_localization_node = launch_ros.actions.Node(
+       package='robot_localization',
+       executable='ekf_node',
+       name='ekf_filter_node',
+       output='screen',
+       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': True}]
     )
+
+    house_spawner = Node(
+        package="autonomous_tb3",
+        output="screen",
+        executable="sdf_spawner",
+        name="maze_spawner",
+       arguments=[house_world_path, "b", "0.0", "0.0"],
+    )
+
+    # Launch async slam toolbox
+    slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("slam_toolbox"),
+                "/launch",
+                "/online_async_launch.py",
+            ]
+        ),
+        launch_arguments={
+            "use_sim_time": "true",
+            "publish_period": "0.0",
+        }.items(),
+    )
+
+    nav2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("nav2_bringup"),
+                "/launch",
+                "/bringup_launch.py",
+            ]
+        ),
+        launch_arguments={"map": map_file, "params_file": params_file}.items(),
+    )
+
+
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        arguments=['-d', default_rviz_config_path],
+        parameters=[{'use_sim_time': True}]
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'fetch', '-topic', 'robot_description', '-x', x_pose, '-y', y_pose],
+        output='screen'
+    )
+    # Teleop node
+    teleop = Node(
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        name='teleop_twist_keyboard',
+        output='screen',
+        prefix='xterm -e'  # This opens a new terminal for teleop input
     )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_fetch_controller],
+            )
+        ),
+        gzserver_cmd,
+        gzclient_cmd,
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
+        robot_state_publisher,
+        house_spawner,
+        slam,
+        #nav2,
+        spawn_entity,
+        robot_localization_node,
+        rviz_node,
+        teleop
+
     ])
